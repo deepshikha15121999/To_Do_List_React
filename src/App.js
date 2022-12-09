ed972dd1936272c3c9c7e105ff827b3246ab6192
deepshikha <101448280+deepshikha15121999@users.noreply.github.com>
2022-12-09 10:37:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function Name(){
+
+    const [first,setfirst]=useState("")
+    const [second,setsecond]=useState([])
+      
+  return(
+    <center>
+    <div id='box'>
+      <table>
+    
+    <input type='text' placeholder='enter name' onChange={(a)=>{
+      setfirst(a.target.value)
+    }}/>
+     
+    <button onClick={()=>{
+      setsecond([...second,first])
+    }}>click</button>
+
+    {
+    second.map((item,index)=>{
+      return(
+        <div>
+      <h1 id='box1'>{item}</h1>
+
+      <button id='del' onClick={()=>{
+        let newlist=[...second]
+        newlist.splice(index,1)
+        setsecond(newlist)
+
+      }}>DELETE</button>
+
+      
+        <button  id='edit'onClick={()=>{
+        let array1=[...second]
+        let newarray=prompt('enter name')
+        if(newarray.trim().length>0){
+          array1[index]=newarray
+        }
+        else{
+          setsecond(array1)
+        }
+        array1[index]=newarray
+        setsecond(array1)
+      }}>EDIT</button>
+
+
+      </div>
+      )
+    })
+  }
+    </table>
     </div>
-  );
-}
+    </center>
+  )
+} 
 
-export default App;
